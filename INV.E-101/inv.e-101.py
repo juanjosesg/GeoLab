8cc30bd1b8f7914a189435dc121289c68508d6a0
juanjosesg <jjsepulvedag@unal.edu.co>
2022-05-15 15:46:37 -0500

    First file created Soil and rock investigation for engineering purposes

---
+++ INV.E-101/inv.e-101.py
@@ -0,0 +1,17 @@
+'''
+SOIL AND ROCK INVESTIGATION FOR ENGINEERING PURPORSES
+--------------------------------------------------------------------------
+Created by    : Juan José Sepúlveda García (jjsepulvedag@unal.edu.co)
+University    : Universidad Nacional de Colombia (unal.edu.co)
+--------------------------------------------------------------------------
+First version : May 2022
+--------------------------------------------------------------------------
+Based on:
+1. I.N.V.E - 101:  Investigación de suelos y rocas para propósitos de ingeniería
+2. ASTM D420 - 98 (2018): Standard Guide for Site Characterization for 
+   Engineering Design and Construction Purposes
+--------------------------------------------------------------------------
+'''
+
+
+
